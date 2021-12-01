d7e0aa874c950ee81918ee830e4de1d42352358d
AnsonRaphael <anson550220@gmail.com>
2021-12-01 23:17:45 +0530

    Added initial files and Beer Dto

---
+++ src/main/java/com/nanos/msscbrewery/web/model/BeerDto.java
@@ -0,0 +1,18 @@
+package com.nanos.msscbrewery.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BeerDto {
+    private UUID uuid;
+    private String beerName;
+    private String beerStyle;
+    private Long upc;
+}
